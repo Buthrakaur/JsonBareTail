2d9dd509bba761bc50b1730cbdd0f719c6b541e1
Filip Kinský <filip@filovo.net>
2017-01-08 13:03:42 +0100

    added dummy components JsonRowList, JsonRow

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,5 +6,28 @@ import styles from './Home.css';
 
 export default class Home extends Component {
   render() {
-	}
+  	return <JsonRowList/>
+  }
+}
+
+class JsonRowList extends React.Component {
+  render() {
+  	var rows = [];
+	//this.props.records
+	[{a:1, b:2, c: 'ccc'}, {a:2, b:3, c: 'ddd'}]
+	.forEach(function(json) {
+      rows.push(<JsonRow json={json.b} key={json.a} />);
+    });
+    return (
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+    );  
+  }	
+}
+
+class JsonRow extends React.Component {
+  render() {
+    return <tr><td>{this.props.json}</td></tr>;
+  }
 }
